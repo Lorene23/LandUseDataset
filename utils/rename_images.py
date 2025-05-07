183e8ff3cf014aa0d1d5bebea2a7a124c8651055
Lorene Paula Catalvas <paulaaquilescatalvas@gmail.com>
2025-05-07 18:17:43 +0800

    Add raw dataset

--- utils/rename_images.py
+++ utils/rename_images.py
@@ -7,14 +7,14 @@ import glob
 def rename_images(path, category):
     """ Standardized the filenames of the final samples. """
 
-    images = glob.glob(f"{path}/{category}/*.jpg")
+    images = glob.glob(f"{path}/{category}/*.jp*g")
     for i, filepath in enumerate(images):
         counter = str(i+1).zfill(4)
         basepath = os.path.dirname(filepath)
-    filename = os.path.basename(filepath)
-    print(f" - {category}/{filename} --> {counter}.jpg")
-    new_filepath = f"{basepath}/{counter}.jpg"
-    os.rename(filepath, new_filepath)
+        filename = os.path.basename(filepath)
+        print(f" - {category}/{filename} --> {counter}.jpg")
+        new_filepath = f"{basepath}/{counter}.jpg"
+        os.rename(filepath, new_filepath)
     
 def main():
     """ Rename image files. """
